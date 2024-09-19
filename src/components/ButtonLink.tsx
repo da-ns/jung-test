bf744aad1f02ac60a23512e048901404f7ad9c99
da-ns <davydova.n.s@ya.ru>
2024-09-19 20:02:09 +0300

    styled button link

---
+++ src/components/ButtonLink.tsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+
+type ButtonLinkProps = {
+    children: React.ReactNode;
+    className?: string;
+    href?: string;
+}
+
+const ButtonLink = ({children, className, href} : ButtonLinkProps) => {
+    return (
+        <a href={href} className={
+                    `px-4 py-2 bg-gray-600 
+                     border border-transparent rounded-md 
+                     font-semibold text-xs text-white uppercase tracking-widest 
+                     hover:bg-indigo-600 active:bg-indigo-900 
+                     focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 
+                     transition duration-150 ` + className
+                }>
+            {children}
+        </a>
+    )
+}
+
+export default ButtonLink
